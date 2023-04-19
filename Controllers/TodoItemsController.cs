acfd0068a744ec19bf8bd854ae57556d0dd7a351
lucas lima <ljesus@reconcavo.org.br>
2023-04-19 11:10:06 -0300

    feat: att controllers

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -9,7 +9,7 @@ using todoApi.Models;
 
 namespace todoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/TodoItems")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
