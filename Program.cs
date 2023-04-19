f92538d2413696684cc74ecd05b08d7ecbeb6e8a
lucas lima <ljesus@reconcavo.org.br>
2023-04-19 10:56:27 -0300

    feat: att post call

--- Program.cs
+++ Program.cs
@@ -3,17 +3,14 @@ using todoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.addDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
